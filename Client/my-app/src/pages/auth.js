2a4868fb4e07827bdf9a01f58f8e875666294b91
J3NKO <callumclaytonjenkins148@outlook.com>
2024-12-03 09:01:20 +0000

    Added Second Route for Logging in but need to debug

--- Client/my-app/src/pages/auth.js
+++ Client/my-app/src/pages/auth.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import {useCookies} from "react-cookie";
 //using axios rather than nodes built in API fetching
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 
 export const Auth = () => {
@@ -20,12 +22,41 @@ const Login = () => {
     const [username, SetUsername] = useState("");
     const [password, SetPassword] = useState("");
 
+    //navigation hook rather than using window object/function
+    const navigate = useNavigate();
+
     const label = "Login";
 
-    return <Form label={label} username={username} SetUsername={SetUsername} 
+    const [_, setCookies] = useCookies(["access_token"]);
+
+    const onSubmit = async(e) => {
+
+        e.preventDefault();
+
+        try{
+
+            const response = await axios.post("http://localhost:3001/auth/login", {
+                username, 
+                password
+            });
+
+            setCookies("access_token", response.data.token);
+            window.localStorage.setItem("userID", response.data.UserID)
+
+            navigate("/home");
+
+        }catch(err) {
+
+            console.error(err);
+
+        };
+
+
+    };
+
+
+    return <Form onSubmit={onSubmit} label={label} username={username} SetUsername={SetUsername} 
     password={password} SetPassword={SetPassword}></Form>
-
-
      
 
 };
@@ -76,7 +107,7 @@ const Register = () => {
         </div>
         <div className="form-group">
             <label htmlFor="paswword">Password: </label>
-            <input type="text" id="password" onChange={(e) => {SetPassword(e.target.value)}}></input>
+            <input type="password" id="password" onChange={(e) => {SetPassword(e.target.value)}}></input>
         </div>
 
         <button type="submit">{label}</button>
